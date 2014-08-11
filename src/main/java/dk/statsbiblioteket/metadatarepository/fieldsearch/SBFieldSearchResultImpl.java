7330e518974762b25a33fddca71a314d1767b74c
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2014-08-11 14:54:28 +0200

    Now with searching enabled

---
+++ src/main/java/dk/statsbiblioteket/metadatarepository/fieldsearch/SBFieldSearchResultImpl.java
@@ -0,0 +1,74 @@
+package dk.statsbiblioteket.metadatarepository.fieldsearch;
+
+import org.fcrepo.server.search.FieldSearchResult;
+import org.fcrepo.server.search.ObjectFields;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SBFieldSearchResultImpl implements FieldSearchResult {
+
+    List<ObjectFields> pids;
+    long cursor = 0;
+
+    private long listOffset;
+    private long completeSize;
+
+    public SBFieldSearchResultImpl(List<String> pids, long cursor, long listOffset, long completeSize) {
+
+        this.pids = convert(pids);
+        this.cursor = cursor;
+        this.listOffset = listOffset;
+        this.completeSize = completeSize;
+    }
+
+    public SBFieldSearchResultImpl(ResultSet m_resultSet) throws SQLException {
+        listOffset = 0;
+        pids = new ArrayList<ObjectFields>();
+        while (m_resultSet.next())  {
+            ObjectFields element = new ObjectFields();
+            element.setPid(m_resultSet.getString("pid"));
+            pids.add(element);
+        }
+        completeSize = pids.size();
+        listOffset = 0;
+    }
+
+    private List<ObjectFields> convert(List<String> pids) {
+        ArrayList<ObjectFields> result = new ArrayList<ObjectFields>(pids.size());
+        for (String pid : pids) {
+            ObjectFields element = new ObjectFields();
+            element.setPid(pid);
+            result.add(element);
+        }
+        return result;
+    }
+
+    @Override
+    public List<ObjectFields> objectFieldsList() {
+        return pids;
+    }
+
+    @Override
+    public String getToken() {
+        return null;
+    }
+
+    @Override
+    public long getCursor() {
+        return cursor;
+    }
+
+    @Override
+    public long getCompleteListSize() {
+        return pids.size();
+    }
+
+    @Override
+    public Date getExpirationDate() {
+        return null;
+    }
+}
--- src/main/java/dk/statsbiblioteket/metadatarepository/fieldsearch/SBFieldSearchSQLImpl.java
+++ src/main/java/dk/statsbiblioteket/metadatarepository/fieldsearch/SBFieldSearchSQLImpl.java
@@ -1,9 +1,16 @@
 package dk.statsbiblioteket.metadatarepository.fieldsearch;
 
 import org.fcrepo.server.errors.ObjectIntegrityException;
+import org.fcrepo.server.errors.RepositoryConfigurationException;
 import org.fcrepo.server.errors.ServerException;
 import org.fcrepo.server.errors.StorageDeviceException;
+import org.fcrepo.server.errors.StreamIOException;
+import org.fcrepo.server.errors.UnrecognizedFieldException;
+import org.fcrepo.server.search.Condition;
+import org.fcrepo.server.search.FieldSearchQuery;
+import org.fcrepo.server.search.FieldSearchResult;
 import org.fcrepo.server.search.FieldSearchSQLImpl;
+import org.fcrepo.server.search.Operator;
 import org.fcrepo.server.storage.ConnectionPool;
 import org.fcrepo.server.storage.DOReader;
 import org.fcrepo.server.storage.RepositoryReader;
@@ -16,6 +23,7 @@ import org.slf4j.LoggerFactory;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -23,17 +31,26 @@ public class SBFieldSearchSQLImpl extends FieldSearchSQLImpl {
 
     private static final Logger logger = LoggerFactory.getLogger(SBFieldSearchSQLImpl.class);
     private final ConnectionPool m_cPool;
+    private final RepositoryReader m_repoReader;
+    private final int m_maxResults;
+    private final int m_maxSecondsPerSession;
 
     public SBFieldSearchSQLImpl(ConnectionPool cPool, RepositoryReader repoReader, int maxResults,
                                 int maxSecondsPerSession) {
         super(cPool, repoReader, maxResults, maxSecondsPerSession);
         m_cPool = cPool;
+        m_repoReader = repoReader;
+        m_maxResults = maxResults;
+        m_maxSecondsPerSession = maxSecondsPerSession;
     }
 
     public SBFieldSearchSQLImpl(ConnectionPool cPool, RepositoryReader repoReader, int maxResults,
                                 int maxSecondsPerSession, boolean indexDCFields) {
         super(cPool, repoReader, maxResults, maxSecondsPerSession, indexDCFields);
         m_cPool = cPool;
+        m_repoReader = repoReader;
+        m_maxResults = maxResults;
+        m_maxSecondsPerSession = maxSecondsPerSession;
     }
 
     @Override
@@ -55,7 +72,6 @@ public class SBFieldSearchSQLImpl extends FieldSearchSQLImpl {
             if (dcmd != null) {
                 InputStream in = dcmd.getContentStream();
                 DCFields dc = new DCFields(in);
-
                 delete = conn.prepareStatement("DELETE FROM doIdentifiers WHERE pid=?");
                 delete.setString(1, pid);
                 delete.executeUpdate();
@@ -90,7 +106,6 @@ public class SBFieldSearchSQLImpl extends FieldSearchSQLImpl {
 
     @Override
     public boolean delete(String pid) throws ServerException {
-
         logger.debug("Entering update(DOReader)");
         Connection conn = null;
         PreparedStatement st = null;
@@ -117,5 +132,61 @@ public class SBFieldSearchSQLImpl extends FieldSearchSQLImpl {
                 logger.debug("Exiting update(DOReader)");
             }
         }
+    }
+
+    @Override
+    public FieldSearchResult findObjects(String[] resultFields, int maxResults, FieldSearchQuery query) throws
+                                                                                                        UnrecognizedFieldException,
+                                                                                                        ObjectIntegrityException,
+                                                                                                        RepositoryConfigurationException,
+                                                                                                        StreamIOException,
+                                                                                                        ServerException,
+                                                                                                        StorageDeviceException {
+        List<Condition> conditions = query.getConditions();
+
+        /*If
+        * only one condition
+        * condition is on dcIdentifier
+        * operator is EQ
+        * value is value*/
+        if (resultFields.length == 1 && resultFields[0].equals("pid")) {
+            if (conditions.size() == 1) {
+                Condition condition = conditions.get(0);
+                if (condition.getProperty().equals("identifier")) {
+                    if (condition.getOperator() == Operator.EQUALS || (condition.getOperator() == Operator.CONTAINS && !condition
+                            .getValue()
+                            .contains("*") && !condition.getValue().contains("?"))) {
+                        PreparedStatement m_statement = null;
+                        ResultSet m_resultSet = null;
+                        Connection conn = null;
+                        try {
+                            conn = m_cPool.getReadOnlyConnection();
+                            m_statement = conn.prepareStatement(
+                                    "SELECT doIdentifiers.pid FROM doIdentifiers where doIdentifiers.dcIdentifier=?");
+                            m_statement.setString(1, condition.getValue());
+                            m_resultSet = m_statement.executeQuery();
+                            return new SBFieldSearchResultImpl(m_resultSet);
+                        } catch (SQLException e) {
+                            throw new StorageDeviceException("Error querying sql db: " + e.getMessage(), e);
+                        } finally {
+                            try {
+                                if (m_resultSet != null) {
+                                    m_resultSet.close();
+                                }
+                                if (m_statement != null) {
+                                    m_statement.close();
+                                }
+                                if (conn != null) {
+                                    m_cPool.free(conn);
+                                }
+                            } catch (SQLException e) {
+                                logger.warn("SQL error during failed query cleanup", e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return super.findObjects(resultFields, maxResults, query);
     }
 }
